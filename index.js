d3c99ca5a533e1bbf478af40860e99bbec104587
Lok Tin Wong <loktinw0620@gmail.com>
2026-07-22 15:40:09 -0700

    Implemented !orderrandom

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 
+var DRINKS = ['Beer', 'Vodka', 'Rum', 'Whisky', 'Wine'];
+
+// Not currently in use
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -9,15 +16,22 @@ const client = new Client({
     ]
 });
 
-client.on('ready', () => {
+client.on('clientReady', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', (message) => {
+    // Ignore messages from other bots
     if (message.author.bot) return;
 
     if (message.content === '!ping') {
         message.reply('Pong!');
+    }
+
+    if (message.content === '!orderrandom') {
+        randomDrink = DRINKS[Math.floor(Math.random() * DRINKS.length)];
+
+        message.reply(`You have ordered a glass of ${randomDrink}.`);
     }
 });
 
